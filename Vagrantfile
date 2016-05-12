8c5b6c24727f129f746c6d3d013d4452ae3b726f
Joe Lauer <joe@lauer.bz>
2016-05-11 23:39:39 -0400

    Support for MySQL 5.7

--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,12 @@
 Vagrant.configure(2) do |config|
 
-
-  config.vm.define "ubuntu1404", primary: true do |guest|
+  config.vm.define "ubuntu14", primary: true do |guest|
     guest.vm.box = "minimal/trusty64"
 
-    config.vm.provision "shell", path: "linux/bootstrap-java8.sh", args: "server-jre"
+    #config.vm.provision "shell", path: "linux/bootstrap-java8.sh", args: "server-jre"
+
+    config.vm.provision "shell", path: "linux/bootstrap-mysql57.sh", args: "test"
+    config.vm.network "forwarded_port", guest: 3306, host: 3306, auto_correct: true
   end
 
 
---
+++ linux/bootstrap-mysql57.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+MYSQL_ROOT_PASSWORD="$1"
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then MYSQL_ROOT_PASSWORD="test"; fi
+
+MYSQL_VERSION="5.7.12"
+
+echo "Installing mysql $MYSQL_VERSION..."
+
+# mysql common
+apt-get update
+apt-get -y install libaio1 libnuma1 apparmor libmecab2 psmisc
+wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-common_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+dpkg -i mysql-common_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+
+# mysql client
+wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-community-client_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+dpkg -i mysql-community-client_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+
+# mysql client #2
+wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-client_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+dpkg -i mysql-client_5.7.12-1ubuntu14.04_amd64.deb
+
+# mysql server
+wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-community-server_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+echo "mysql-community-server  mysql-community-server/re-root-pass     password	$MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+echo "mysql-community-server  mysql-community-server/root-pass        password	$MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+echo "mysql-community-server  mysql-community-server/remove-data-dir  boolean true"  | sudo debconf-set-selections
+dpkg -i mysql-community-server_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+
+# mysql server #2
+wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-server_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+dpkg -i mysql-server_$MYSQL_VERSION-1ubuntu14.04_amd64.deb
+
+# by default mysql only allows localhost (not via port forward)
+echo "DELETE FROM mysql.user WHERE NOT Host = 'localhost'" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+echo "UPDATE mysql.user SET Host='%' where Host='localhost'" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+echo "GRANT ALL PRIVILEGES ON *.* TO root@localhost" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+echo "FLUSH PRIVILEGES" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+
+# port forwards only work to a real ip, not localhost
+sed -i "s/= 127.0.0.1/= 0.0.0.0/" /etc/mysql/my.cnf
+
+service mysql restart
+
+echo "Installed mysql $MYSQL_VERSION"
