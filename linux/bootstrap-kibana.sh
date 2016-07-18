6246c17b7866bbfe717eb73f5aa398ab76adb332
Garth Patil <garth@tunnel19.com>
2016-07-18 09:27:38 -0700

    dont change kibana.yml file

--- linux/bootstrap-kibana.sh
+++ linux/bootstrap-kibana.sh
@@ -37,10 +37,6 @@ dpkg -i kibana_$KB_VERSION\_$ARCH.deb
 echo "Configuring kibana to search $JAVA_HOME for java"
 echo "JAVA_HOME=\"$JAVA_HOME\"" >> /etc/default/kibana
 
-# port forwards only work to a real ip, not localhost
-echo "Configuring kibana to bind to all network interfaces (not just localhost by default)"
-sed -i "s/^.*network\.host.*$/network.host: 0.0.0.0/" /etc/kibana/kibana.yml
+service kibana restart
 
-service kibana4 restart
-
-echo "Installed kibana4 $KB_VERSION"
+echo "Installed kibana $KB_VERSION"
