9bed94d46755198e3df49a0c0086f9f1f3afbc80
Joe Lauer <joe@lauer.bz>
2020-05-19 14:29:13 -0400

    Script to provision openjdk8

--- linux/bootstrap-openjdk8.sh
+++ linux/bootstrap-openjdk8.sh
@@ -10,41 +10,41 @@ fi
 mkdir -p "$DOWNLOAD_DIR"
 
 # defaults
-JAVA_JVM="hotspot"
-JAVA_VERSION="222"
+JAVA_TYPE="jre"     		# jdk, jre, or server-jre
+JAVA_VERSION="8u252"
 
 # arguments
 for i in "$@"; do
   case $i in
-    --jvm=*)
-      JAVA_JVM="${i#*=}"
+    --type=*)
+      JAVA_TYPE="${i#*=}"
       ;;
     --version=*)
       JAVA_VERSION="${i#*=}"
       ;;
     *)
       echo "Unknown argument '$i'"
-      echo "--jvm=[hotspot|openj9] --version=[222|...]"
+      echo "--type=[jdk|jre] --version=[8u252|...]"
       exit 1  
       ;;
   esac
 done
 
 # java type valid?
-case $JAVA_JVM in
-  hotspot|openj9)
+case $JAVA_TYPE in
+  jdk|jre)
     ;;
   *)
-    echo "Unsupported java jvm $JAVA_JVM (must be hotspot, or openj9)"
+    echo "Unsupported java type $JAVA_TYPE (must be jdk, jre, or server-jre)"
     exit 1
     ;;
 esac
 
 # java versions valid? (we have to setup stuff so versions must be manually supported)
 case $JAVA_VERSION in
-  222)
-    JAVA_FILE_VERSION="b10"
-    OJ9_FILE_VERSION="_openj9-0.15.1"
+  8u252)
+    JAVA_FILE_VERSION="8u252b09"
+    JAVA_URL_DIR="jdk8u252-b09"
     ;;
   *)
     echo "Unsupported java version $JAVA_VERSION (you'll need to add code to this script to correctly install it)"
@@ -52,7 +52,7 @@ case $JAVA_VERSION in
     ;;
 esac
 
-# https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u222b10.tar.gz
+# https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jre_x64_linux_hotspot_8u252b09.tar.gz
 
 # dependencies
 if type apt-get &>/dev/null; then
@@ -63,33 +63,29 @@ elif type yum &>/dev/null; then
   sudo yum -y install curl
 fi
 
-echo "Installing Java 8-$JAVA_VERSION ($JAVA_JVM)..."
-
-TARGET_SUFFIX=""
-if [ "$JAVA_JVM" = "openj9" ]; then
-  JAVA_OJ9=$OJ9_FILE_VERSION
-  TARGET_SUFFIX="-openj9"
-fi
+echo "Installing $JAVA_TYPE $JAVA_VERSION..."
 
 # download file if it doesn't exist yet
-JAVA_TARBALL_FILE="OpenJDK8U-jdk_x64_linux_${JAVA_JVM}_8u${JAVA_VERSION}${JAVA_FILE_VERSION}${JAVA_OJ9}.tar.gz"
-JAVA_URL="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u$JAVA_VERSION-$JAVA_FILE_VERSION${JAVA_OJ9}/$JAVA_TARBALL_FILE"
+JAVA_TARBALL_FILE="OpenJDK8U-${JAVA_TYPE}_x64_linux_hotspot_${JAVA_FILE_VERSION}.tar.gz"
+JAVA_URL="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/$JAVA_URL_DIR/$JAVA_TARBALL_FILE"
+
 if [ ! -f "$DOWNLOAD_DIR/$JAVA_TARBALL_FILE" ]; then
   echo "Downloading $JAVA_URL"
   curl -o "$DOWNLOAD_DIR/$JAVA_TARBALL_FILE" -j -k -L "$JAVA_URL"
 fi
 
-rm -Rf ./jdk8u${JAVA_VERSION}-${JAVA_FILE_VERSION}
+JAVA_DIR=$JAVA_URL_DIR
+if [ "$JAVA_TYPE" = "jre" ]; then
+  JAVA_DIR=$JAVA_DIR-jre
+fi
 
+# jdk8u252-b09-jre
 tar zxvf $DOWNLOAD_DIR/$JAVA_TARBALL_FILE
-
 mkdir --parents /usr/lib/jvm
 rm -f /usr/lib/jvm/current
-# jdk8u222-b10
-TARGET_DIR=openjdk-1.8.0_${JAVA_VERSION}${TARGET_SUFFIX}
-rm -Rf /usr/lib/jvm/${TARGET_DIR}
-mv ./jdk8u${JAVA_VERSION}-${JAVA_FILE_VERSION} /usr/lib/jvm/${TARGET_DIR}
-ln -s /usr/lib/jvm/${TARGET_DIR} /usr/lib/jvm/current
+rm -Rf /usr/lib/jvm/jdk$JAVA_VERSION
+mv $JAVA_DIR /usr/lib/jvm/
+ln -s /usr/lib/jvm/$JAVA_DIR /usr/lib/jvm/current
 
 # does /etc/environment exist?
 if [ -f /etc/environment ]; then
